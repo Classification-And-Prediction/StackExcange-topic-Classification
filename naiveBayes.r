34dff6fe47cb800a343528a6ee084e9d0a843586
Aayush Kumar Singha <aayushsingh.mail@gmail.com>
2015-11-24 06:59:18 +0530

    naiveBayes.r

--- naiveBayes.r
+++ naiveBayes.r
@@ -5,18 +5,11 @@ library(MASS)
 library(caret)
 library(SnowballC)
 
-train = read.table("train_set.csv", sep="\t", header=T)
-test = read.table("test_set.csv", sep="\t", header=T)
+train = read.table("train_set.csv", sep="\t", header=F)
+test = read.table("test_set.csv", sep="\t", header=F)
 
-#pred <- train$pred
-#hw <- subset(train, train$class==1)
-#sw <- subset(train, train$class==0)
-
-#train$set = "Hardware"
-#train[train$class==0,]$set = "Software"
-#train$set = as.factor(train$set)
-
-#nb = NaiveBayes(train[ ,train$pred], train$set)
+names(train) <- c('class','pred')
+names(test) <- c('pred')
 
 #stopwords
 mystopwords <- c(stopwords("english"),"week","arduino","words","need","get","will","want","know","work","also")
@@ -31,13 +24,8 @@ train.corpus <- tm_map(train.corpus, removeWords, mystopwords)
 train.corpus <- tm_map(train.corpus, stemDocument)
 train.corpus <- tm_map(train.corpus, removeWords, "(http)\\w+")
 train.corpus <- tm_map(train.corpus, removeWords, "\\b[a-zA-Z0-9]{10,100}\\b")
-#train.corpus <- gsub('(http)\\w+',"",train.corpus)
 train.corpus.dtm <- DocumentTermMatrix(train.corpus, control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE), stopwords = TRUE, removePunctuation=TRUE))
 train.corpus.dtms <- removeSparseTerms(train.corpus.dtm, 0.935)
-
-#TermDocumentMatrix(train.corpus)
-#inspect(train.corpus.dtm)
-#findFreqTerms(train.corpus.dtm, N)   #N <- freq
 
 #corpus for test set
 test.corpus <- Corpus(VectorSource(test$pred))
@@ -52,40 +40,30 @@ test.corpus <- tm_map(test.corpus, removeWords, "\\b[a-zA-Z0-9]{10,100}\\b")
 test.corpus.dtm <- DocumentTermMatrix(test.corpus, control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE), stopwords = TRUE, removePunctuation=TRUE))
 test.corpus.dtms <- removeSparseTerms(test.corpus.dtm, 0.935) 
 
-
 m <- as.matrix(train.corpus.dtms)
 n <- as.matrix(test.corpus.dtms)
-#v <- sort(rowSums(m), decreasing=TRUE)
 
-#model <- naiveBayes(set ~ pred, data = train)
-#pred <- predict(model, train[495:510,])
-#pred1 <- predict(model, newdata = data.frame(pred="This post is related to Software"), type="raw")
-#pred2 <- predict(model, newdata = data.frame(pred="Hardware"), type="raw")
-#pred3 <- predict(model, newdata = test[1,], type="raw")
-#predict(model, newdata = data.frame(Sepal.Width = 3), type=('raw'))
-#table(pred, test)
-
-#Train model
-#tune.control <- tune.control(random =F, nrepeat=1, repeat.aggregate=min,sampling=c("cross"),sampling.aggregate=mean, cross=10, best.model=T, performances=T) 
-#model <- naiveBayes(m,as.factor(train$class),tune.control);
-
-#Prediction
-
-#"my.predict.NB"<-function(object,newdata){ 
-#results <- predict(model,n[10:20,])
-#} 
-#test <- chisq.test(as.factor(train$class)[496:505], results)
-#print(test)
-#tune(naiveBayes,m,as.factor(train$class),fun=my.predict.NB)
-
-#mat <- table(results,n[10:20])
-#write.table(mat, file="confusion_matrix.txt", row.names=FALSE, col.names=FALSE)
-#write.matrix(mat,'confusion_matrix.txt',sep = "\t")
-
+#cross-validation
 fitcontrol <- trainControl(method="cv", 10)
-#set.seed(825)
+set.seed(825)
 nb <- train(m, as.factor(train$class), method="nb", tuneGrid=data.frame(.fL=1, .usekernel=FALSE), trControl=fitcontrol)
 
+#cv results
 print(nb$results)
-
 print(nb$resample)
+
+#Train model
+model <- naiveBayes(m,as.factor(train$class));
+
+#Prediction
+results <- predict(model,n[10:20,])
+
+for(i in 10:20)
+{	
+	pr <- results[i-9]
+	pt <- test$pred[i]
+	print(i)
+	print(pr)
+	print(pt)
+	print("=================================================================================================================")
+}
